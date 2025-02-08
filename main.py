9fdd0c3b7524160f331caf6a80b2c3d1270dae84
rudra144 <147151654+rudra144@users.noreply.github.com>
2025-02-08 21:29:20 +0100

    backend updated

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 from prediction_helper import predict
 
--- prediction_helper.py
+++ prediction_helper.py
@@ -1,10 +1,14 @@
+
 import pandas as pd
 import joblib
+import os
 
-model_young = joblib.load("artifacts/model_young.joblib")
-model_rest = joblib.load("artifacts/model_rest.joblib")
-scaler_young = joblib.load("artifacts/scaler_young.joblib")
-scaler_rest = joblib.load("artifacts/scaler_rest.joblib")
+print("Current Working Directory:", os.getcwd())
+
+model_young = joblib.load("app/artifacts/model_young.joblib")
+model_rest = joblib.load("app/artifacts/model_rest.joblib")
+scaler_young = joblib.load("app/artifacts/scaler_young.joblib")
+scaler_rest = joblib.load("app/artifacts/scaler_rest.joblib")
 
 def calculate_normalized_risk(medical_history):
     risk_scores = {
